007e4d1b4bb044e02d587f1d68fcfde9e6f739c0
berringy <berringyilmaz@gmail.com>
2025-03-16 22:36:36 +0300

    berrinV1

--- src/test/java/getland/stepdefs/Hook.java
+++ src/test/java/getland/stepdefs/Hook.java
@@ -1,31 +1,32 @@
-package getland.utilities;
+package getland.stepdefs;
 
-
+import getland.utilities.Driver;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import getland.utilities.Authentication;
-import getland.utilities.ConfigReader;
+import io.cucumber.java.Scenario;
 
 public class Hook {
 
-    /*
-    Hooks classinda @api tagi ile api testlerine özel bir before method olusturduk
-    Bu sayede base url gibi ayri bir yapiya ihtiyac kalmadi, daha sade oldu
-     @Before("@apie2e") yazdigimizda artik sadece @apie2e tagina sahip olan scenariolar
-     icin özellestirdik
-     */
-    public static RequestSpecification spec;
-
-    @Before("@apie2e")
-    public void setUp() throws Exception {
-        spec = new RequestSpecBuilder()
-                .setBaseUri(ConfigReader.getProperty("baseUrl"))
-                .setContentType(ContentType.JSON)
-                .addHeader("Authorization", "Bearer " + Authentication.generateToken())
-                .build();
+    @Before
+    public void setUp() {
+        System.out.println("Test basladı");
     }
 
 
+    @After
+    public void tearDown(Scenario scenario) {
+        Driver.closeDriver();
+        System.out.println("Test bitti");
+    }
+
+
+
+    /* @After
+    public void tearDown(Scenario scenario) throws Exception {
+        if (scenario.isFailed()) {
+            TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
+            scenario.attach(ts.getScreenshotAs(OutputType.BYTES), "image/png", "scenario" + scenario.getName());
+            Driver.closeDriver();
+        }
+    } */
 }
