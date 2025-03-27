3454e2de7e218afb73461fa09fd2383349ee18c7
serkandezcan <serkandezcan@gmail.com>
2025-03-27 22:31:10 +0300

    Resolved merge conflict in Hook.java

--- src/test/java/getland/stepdefs/ui_stepdefs/Hook.java
+++ src/test/java/getland/stepdefs/ui_stepdefs/Hook.java
@@ -1,21 +1,4 @@
-<<<<<<< HEAD:src/test/java/getland/stepdefs/ui_stepdefs/Hook.java
 package getland.stepdefs.ui_stepdefs;
-
-
-
-
-
-
-
-
-import getland.utilities.Driver;
-import io.cucumber.java.After;
-
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
-
-=======
-package getland.stepdefs;
 
 
 import io.cucumber.java.Before;
@@ -24,7 +7,6 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import getland.utilities.Authentication;
 import getland.utilities.ConfigReader;
->>>>>>> 30a489d737dedca17828ebabb9318d462cb34ab9:src/test/java/getland/stepdefs/Hook.java
 
 public class Hook {
 
