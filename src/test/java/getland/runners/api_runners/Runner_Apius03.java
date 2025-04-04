453bb7b329f7dcecd7b813ae233b80d4961cf414
rahmitugrulduman <rahmitugrulduman@yahoo.com>
2025-04-04 22:27:06 +0300

    deneme3

--- src/test/java/getland/runners/api_runners/Runner_Apius03.java
+++ src/test/java/getland/runners/api_runners/Runner_Apius03.java
@@ -1,4 +1,4 @@
-package getland.runners.api_runner;
+package getland.runners.api_runners;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
